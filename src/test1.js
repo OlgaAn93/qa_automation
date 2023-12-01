36173534d0dcc7c4170687a2715f1fb357e3fb4e
olga.andrieieva <andreeva.hellga@gmail.com>
2023-12-01 17:50:16 +0100

    Data types operations

---
+++ src/test1.js
@@ -0,0 +1,40 @@
+//string
+let season = "winter"
+
+console.log("test1");
+
+console.log(`Now it is ${season}`);
+
+//number
+
+let num1 = 5;
+let num2 = 10;
+
+num2 > num1 ? console.log("number is bigger") : ("number is smaller")
+
+//booleans
+
+let c = true;
+let b = false;
+let result = c || b; 
+console.log(result); 
+
+//arrays
+
+let array1 = [1, 2, 3];
+console.log(array1.reverse);
+
+// object
+
+let user = {
+    name: 'Maria',
+    age: 28,
+    city: 'Berlin'
+};
+
+console.log(user.name);
+
+// Undefinied
+
+let a;
+console.log(a);
